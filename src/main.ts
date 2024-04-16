c1c17ba88a0eae0f41b7fb08802d420142aa87bf
crisgonzalezallaria <143832930+crisgonzalezallaria@users.noreply.github.com>
2024-04-16 18:58:33 -0300

    wip

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ import { exec } from 'child_process'
  * @returns {Promise<void>} Resolves when the action is complete.
  */
 export async function run(): Promise<void> {
-  console.log('RUNNNING')
   try {
     const diff = await execPromise(
       'git diff --name-only remotes/origin/main...HEAD'
